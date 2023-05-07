6384f9955e7c16aa47a86ab1171e40af08dd559a
Rafa <nlider7211@gmail.com>
2023-05-07 19:51:30 +0200

    Avoiding objects

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
@@ -8,14 +9,21 @@ public class EnemyController : MonoBehaviour
     public Animator animator;
     public float movementSpeed;
     public float distanceBetween;
+    private NavMeshAgent agent;
+    private Vector3 target;
     private Rigidbody2D rb;
     private float distance;
     private float cooldown;
     private float lastHit;
     private bool isDead;
+
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
         cooldown = 1.5f;
         lastHit = 2f;
         isDead = false;
@@ -30,15 +38,18 @@ public class EnemyController : MonoBehaviour
             Vector2 direction = player.transform.position - transform.position;
             direction.Normalize();
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-            if (distance < distanceBetween) {
-                rb.velocity = direction * movementSpeed;
+            // target = direction;
+            NavMeshHit hit;
+            if (distance < distanceBetween && !agent.Raycast(player.transform.position, out hit)) {
+                agent.SetDestination(player.transform.position);
+                // SetAgentPosition();
+                // rb.velocity = direction * movementSpeed;
                 // transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, movementSpeed = Time.deltaTime);
                 transform.rotation = Quaternion.Euler(Vector3.forward * angle);
             } else {
                 rb.velocity = new Vector2(0,0);
             }
-            animator.SetFloat("Speed", rb.velocity.magnitude);
+            animator.SetFloat("Speed", agent.velocity.magnitude/agent.speed);
 
             lastHit += Time.deltaTime;
             if (distance < 1.5 && lastHit > cooldown) {
@@ -54,13 +65,15 @@ public class EnemyController : MonoBehaviour
     }
 
     public IEnumerator GetHit() {
-        movementSpeed = 0.0f;
+        agent.SetDestination(transform.position);
+        // movementSpeed = 0.0f;
         isDead = true;
         animator.SetBool("isDead", true);
         animator.Play("EnemyDeadFromBat");
         transform.rotation = transform.rotation * Quaternion.Euler(0, 0, 180);
         rb.bodyType = RigidbodyType2D.Static;
         GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().sortingLayerName = "Ground";
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length+animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
     }
 }
